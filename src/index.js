3810e6a91873acd9abe44b84f96fdeb791639394
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-29 21:02:39 +0100

    feat: add solution and style/layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./styles.css";
+
+const palindromeInput = document.querySelector("#text-input");
+const checkPalindromeBtn = document.querySelector("#check-btn");
+const result = document.querySelector("#result");
+
+function isPalindrome() {
+  result.innerHTML = "";
+  result.className = "result";
+  let inputString = palindromeInput.value;
+  if (inputString == "") {
+    alert("Please input a value");
+  } else {
+    const cleanInputString = inputString.replace(/[_\W]/gi, "").toLowerCase();
+    const reversedString = Array.from(cleanInputString).reverse().join("");
+    if (cleanInputString === reversedString) {
+      result.innerHTML = `<p><strong>${inputString}</strong> is a palindrome.</p>`;
+      result.classList.add("positive");
+    } else {
+      result.innerHTML = `<p><strong>${inputString}</strong> is not a palindrome.</p>`;
+      result.classList.add("negative");
+    }
+  }
+}
+
+checkPalindromeBtn.addEventListener("click", () => {
+  isPalindrome();
+});
